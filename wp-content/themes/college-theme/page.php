c58c58f91f489fc5c68249e01551c98d98fc742a
BaseBenjamin13 <benmorgiewicz@gmail.com>
2025-01-14 20:08:32 -0500

    add header & footer

--- wp-content/themes/college-theme/page.php
+++ wp-content/themes/college-theme/page.php
@@ -1,4 +1,5 @@
 <?php 
+    get_header();
 
     while(have_posts()) {
         the_post(); ?>
@@ -6,4 +7,6 @@
         <h2><?php the_title(); ?></h2>
         <p><?php the_content(); ?></p>
     <?php }
+    
+    get_footer();
 ?>
